f4edbcede19a74584a1cb894d27027da27304dbd
bobrobcpp <king.roob@gmail.com>
2017-09-14 20:10:53 +0100

    Jasmine tests created, README created

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,47 +26,70 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+         it('has a valid URL', function() {
+            allFeeds.forEach(function(feed){
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+
+            });
+         });
+
+            it('has a valid name', function() {
+            allFeeds.forEach(function(feed){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+            });
+         });
+
+    });
+
+    describe('The menu', function() {
+        it('is hidden by default', function(){
+        expect($('body').attr("class")).toMatch("menu-hidden");
+        });
+
+        it('changes visibility when clicked', function(){
+            $('.menu-icon-link').trigger("click");
+            expect($('body').attr("class")).not.toMatch("menu-hidden");
+            $('.menu-icon-link').trigger("click");
+            expect($('body').attr("class")).toMatch("menu-hidden");
+        });
+
+    });
+
+    describe('Initial Entries', function(){
+            beforeEach(function(done){
+                loadFeed(0,function(){
+                    done();
+                });
+            });
+
+            it('has at least one .entry element in its .feed container', function(done){
+                expect($('.feed').length).toBeGreaterThan(0);
+                done();
+            });
+    });
+
+    describe('New Feed Selection', function(){
+        var firstContent;
+        var secondContent;
+                beforeEach(function(done){
+                loadFeed(0,function(){
+                    loadFeed(1,function(){
+                        secondContent = $('.feed').html();
+                               done();
+                    });
+                    firstContent = $('.feed').html();
+                });
+            });
+            it('is loading content that changes', function(done){
+                expect(firstContent).not.toEqual(secondContent);
+                done();
+            });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
-
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 }());
